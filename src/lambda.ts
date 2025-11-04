873b28d5972153fe1f1bb270e90ba71d3959b86d
Tâm Đoàn <tamdoan@tams-mbp.lan>
2025-11-04 09:57:11 +0200

    fix lambda file

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,5 +2,17 @@ import serverlessExpress from '@vendia/serverless-express';
 import app from './app';
 import { connectToDatabase } from './database';
 
-connectToDatabase();
-export const handler = serverlessExpress({ app });
+let dbInitPromise: Promise<unknown> | null = null;
+async function ensureDatabaseConnected() {
+  if (!dbInitPromise) {
+  dbInitPromise = connectToDatabase();
+  }
+  await dbInitPromise;
+}
+
+const lambdaHandler = serverlessExpress({ app });
+
+export const handler = async (event: any, context: any, callback: any) => {
+  await ensureDatabaseConnected();
+  return lambdaHandler(event, context, callback);
+};
